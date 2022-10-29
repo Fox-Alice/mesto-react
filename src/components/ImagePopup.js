da4333dc5ba669a3369d54f6070fc2702330a1f9
Fox Alice <elviratr@yandex.ru>
2022-10-29 03:33:30 +0300

    fix: Optional chaining

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -2,27 +2,26 @@ import React from 'react'
 
 function ImagePopup(props) {
 
-    if (props.item) {
-        return (
-            <div className={`image-popup popup ${props.item && 'popup_opened'}`}>
-                <div className="image-popup__container">
-                    <button
-                        onClick={props.onClose}
-                        className="image-popup__close-icon popup__close-icon"
-                        aria-label="close"
-                        type="button"
+    return (
+        <div className={`image-popup popup ${props?.item && 'popup_opened'}`}>
+            <div className="image-popup__container">
+                <button
+                    onClick={props?.onClose}
+                    className="image-popup__close-icon popup__close-icon"
+                    aria-label="close"
+                    type="button"
+                />
+                <div>
+                    <img
+                        className="image-popup__image"
+                        src={props?.item?.link}
+                        alt={props?.item?.name}
                     />
-                    <div>
-                        <img
-                            className="image-popup__image"
-                            src={props.item.link}
-                            alt={props.item.name}
-                        />
-                        <p className="image-popup__caption">{props.item.name}</p>
-                    </div>
+                    <p className="image-popup__caption">{props?.item?.name}</p>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
 export default ImagePopup;
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,36 +1,34 @@
 import React from 'react'
 
 function PopupWithForm(props) {
-    if (props.active) {
 
-        return (
-            <div className={`popup ${props.name}-popup ${props.active && 'popup_opened'}`}>
-                <div className="popup__container">
+    return (
+        <div className={`popup ${props?.name}-popup ${props?.active && 'popup_opened'}`}>
+            <div className="popup__container">
+                <button
+                    onClick={props?.onClose}
+                    className={`popup__close-icon ${props?.name}-popup__close-icon`}
+                    aria-label="close"
+                    type="button"
+                />
+                <h2 className="popup__title">{props?.title}</h2>
+                <form
+                    className={`popup__form popup__form_type_${props?.name}`}
+                    name={`${props?.name}-popup`}
+                    noValidate=""
+                >
+                    {props?.children}
                     <button
-                        onClick={props.onClose}
-                        className={`popup__close-icon ${props.name}-popup__close-icon`}
-                        aria-label="close"
-                        type="button"
-                    />
-                    <h2 className="popup__title">{props.title}</h2>
-                    <form
-                        className={`popup__form popup__form_type_${props.name}`}
-                        name={`${props.name}-popup`}
-                        noValidate=""
+                        className="popup__button popup__save-button"
+                        aria-label="save"
+                        type="submit"
                     >
-                        {props.children}
-                        <button
-                            className="popup__button popup__save-button"
-                            aria-label="save"
-                            type="submit"
-                        >
-                            {props.buttonText}
-                        </button>
-                    </form>
-                </div>
+                        {props?.buttonText}
+                    </button>
+                </form>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default PopupWithForm;
